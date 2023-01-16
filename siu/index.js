68b3edcc7d5d5f57e344b4145d7ddad0f8055b91
cuncho <96427943+cuncho@users.noreply.github.com>
2023-01-16 22:56:47 +0900

    Add files via upload

---
+++ siu/index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    var imgs = document.querySelectorAll('img');
+    var background = document.querySelectorAll('background-image');
+    var link = document.querySelectorAll('div');
+    imgs.forEach((a, i)=>{
+        a.src = 'https://i1.sndcdn.com/artworks-Ujvq6mL2CeWfvc2i-9yRZpg-t240x240.jpg'
+    })
+    background.forEach((a, i)=>{
+        a.src = 'https://i1.sndcdn.com/artworks-Ujvq6mL2CeWfvc2i-9yRZpg-t240x240.jpg'
+    })
+    background.forEach((a, i)=>{
+        a.src = 'siuuuuuuuuuu'
+    })
+}, 100)
